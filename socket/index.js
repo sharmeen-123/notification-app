926178a2351e736c6f636b20fea3ddb737d180b2
sharmeen-123 <fa20-bcs-088@isbstudent.comsats.edu.pk>
2023-07-17 22:14:34 +0500

    msg

---
+++ socket/index.js
@@ -0,0 +1,41 @@
+import { Server } from "socket.io";
+
+const io = new Server({ 
+    cors:{
+        origin:"http://localhost:3000"
+    }
+});
+
+let onlineUsers = []
+
+const addnewUser = (username, socketId) => {
+  !onlineUsers.some(user=> user.username === username) && 
+  onlineUsers.push({username, socketId})
+}
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter(user => user.socketId !== socketId)
+}
+
+const getUser = (username) => {
+  return (onlineUsers.find(user=> user.username === username))
+}
+
+io.on("connection", (socket) => {
+
+  socket.on("newUser" , (username) => {
+    addnewUser(username,socket.id)
+  })
+  socket.on("sendNotification", ({senderName,recieverName,type})=> {
+    const reciever = getUser(recieverName)
+    io.to(reciever.socketId).emit("getNotification", {
+      senderName,
+      type
+    })
+  })
+  socket.on("disconnect", ()=> {
+    removeUser(socket.id)
+  })
+});
+
+io.listen(5000);
